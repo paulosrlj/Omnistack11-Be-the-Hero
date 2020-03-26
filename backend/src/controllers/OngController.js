a9f7b9d99022e82e8e58d0f9181a2948ee620f58
Paulo <paulo.leite@academico.ifpb.edu.br>
2020-03-26 16:55:44 -0300

    Front-end e alterações

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,18 +1,18 @@
-const crypto = require('crypto')
-const connection = require('../database/connection')
+const crypto = require('crypto');
+const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const ongs = await connection('ongs').select('*')
+        const ongs = await connection('ongs').select('*');
 
-        return response.json(ongs)
+        return response.json(ongs);
     },
 
     async create(request, response) {
-        const { name, email, whatsapp, city, uf } = request.body
+        const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX')
-
+        const id = crypto.randomBytes(4).toString('HEX');
+        
         await connection('ongs').insert({
             id,
             name,
@@ -20,9 +20,8 @@ module.exports = {
             whatsapp,
             city,
             uf
-        })
+        });
 
-
-        return response.json({ id })
+        return response.json({ id });
     }
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const routes = require('./routes')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const routes = require('./routes');
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333)
+app.listen(3333);
